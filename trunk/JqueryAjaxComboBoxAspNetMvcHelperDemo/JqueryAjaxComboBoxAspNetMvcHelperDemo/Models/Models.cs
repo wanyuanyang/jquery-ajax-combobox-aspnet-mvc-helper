13b860691ad05db5495e7218a7a4ef65c7191d08
i.enable.much@gmail.com <i.enable.much@gmail.com@34b718d6-f5cb-dab4-daaa-9a4ce85cfa30>
2011-08-06 15:29:31 +0000

    Refactor code

--- trunk/JqueryAjaxComboBoxAspNetMvcHelperDemo/JqueryAjaxComboBoxAspNetMvcHelperDemo/Models/Models.cs
+++ trunk/JqueryAjaxComboBoxAspNetMvcHelperDemo/JqueryAjaxComboBoxAspNetMvcHelperDemo/Models/Models.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using FluentNHibernate.Mapping;
-    using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 
 public class Product
