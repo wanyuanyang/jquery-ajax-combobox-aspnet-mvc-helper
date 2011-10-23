b917a9892c55f94811b7f426fa42cd4144706761
i.enable.much@gmail.com <i.enable.much@gmail.com@34b718d6-f5cb-dab4-daaa-9a4ce85cfa30>
2011-10-23 07:17:28 +0000

    Added string-based name for helpers

--- trunk/JqueryAjaxComboBoxAspNetMvcHelperDemo/JqueryAjaxComboBoxAspNetMvcHelperDemo/Controllers/ProductController.cs
+++ trunk/JqueryAjaxComboBoxAspNetMvcHelperDemo/JqueryAjaxComboBoxAspNetMvcHelperDemo/Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 using NHibernate.Linq;
 using JqueryAjaxComboBoxAspNetMvcHelperDemo.Models;
+using Ienablemuch.JqueryAjaxComboBox;
 
 
 namespace JqueryAjaxComboBoxAspNetMvcHelperDemo.Controllers
@@ -54,7 +55,8 @@ namespace JqueryAjaxComboBoxAspNetMvcHelperDemo.Controllers
         { 
             using(var s = SessionFactoryBuilder.GetSessionFactory().OpenSession())
             {
-                return View("Input", s.Get<Product>(id));
+                var p = s.Get<Product>(id);                
+                return View("Input", p);
             }
         }
 
@@ -78,8 +80,41 @@ namespace JqueryAjaxComboBoxAspNetMvcHelperDemo.Controllers
             
         }
 
+        public JsonResult TesterA(string q_word, string primary_key, int per_page, int page_num, string cascaded_word)
+        {
+            return Json(new { q_word, primary_key, per_page, page_num, cascaded_word}, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult TesterB([Bind(Include = "q_word, primary_key, per_page, page_num, cascaded_word")]LookupRequest lr)
+        {
+            return Json(new { lr.q_word, lr.primary_key, lr.per_page, lr.page_num, lr.cascaded_word }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public class Person
+        {
+            public string Lastname { get; set; }
+            public string Firstname { get; set; }
+            public string Middlename{ get; set; }
+        }
+
+        public JsonResult TesterC(string Lastname, string Firstname, string Middlename)
+        {
+            return Json(new { Lastname, Firstname, Middlename }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult TesterD(Person p)
+        {
+            return Json(new { p.Lastname, p.Firstname, p.Middlename }, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+
         [HttpPost]
-        public JsonResult Lookup(string cascaded_word, string q_word, string primary_key, int per_page, int page_num)
+        public JsonResult xLookup(string cascaded_word, string q_word, string primary_key, int per_page, int page_num)
         {
 
 
@@ -139,6 +174,82 @@ namespace JqueryAjaxComboBoxAspNetMvcHelperDemo.Controllers
 
             }//using
         }//CascadedFromCategoryLookup
+
+
+        [HttpPost]
+        public JsonResult Lookup(LookupRequest lr)
+        {
+
+            // lr.q_word = Request["q_word"];
+            
+            
+
+            using (var svc = SessionFactoryBuilder.GetSessionFactory().OpenSession())
+            {
+
+                int categoryId = 0;
+
+                bool isNumber = int.TryParse(lr.cascaded_word, out categoryId);
+
+
+                /* categoryId = 0; */
+                /*lr.per_page = 5;
+                lr.page_num = 1;*/
+                // lr.q_word = lr.q_word ?? "";
+
+                lr.q_word = lr.q_word ?? "";
+
+                var FilteredProduct = svc.Query<Product>()
+                                        .Where(x =>
+                                            (
+
+                                                categoryId == 0
+                                                ||
+                                                (categoryId != 0 && x.Category.CategoryId == categoryId)
+
+                                            )
+
+                                            &&
+
+                                            (lr.q_word == "" || x.ProductName.Contains(lr.q_word))
+
+                                            );
+
+
+                var PagedFilter = FilteredProduct.OrderBy(x => x.ProductName)
+                                  .LimitAndOffset(lr.per_page, lr.page_num)
+                                  .Fetch(x => x.Category)
+                                  .ToList();
+
+                return Json(
+                    new
+                    {
+                        cnt = FilteredProduct.Count()
+
+                        ,
+                        primary_key = PagedFilter.Select(x => x.ProductId)
+
+                        ,
+                        candidate = PagedFilter.Select(x => x.ProductName)
+
+                        ,
+                        cnt_page = PagedFilter.Count()
+
+                        ,
+                        attached = PagedFilter.Select(x =>
+                              new[]
+                                {
+                                    new string[] { "Product Code", x.ProductCode },                                    
+                                    new string[] { "Category Code", x.Category.CategoryCode },
+                                    new string[] { "Category", x.Category.CategoryName }
+                                }
+                          )
+                    }
+                    );
+
+            }//using
+        }//CascadedFromCategoryLookup
+
 
         [HttpPost]
         public string Caption(string q_word)
